d356a565f51be96d5b850b13bede68f81b5a8544
TaeUk471 <asky990223@gmail.com>
2024-09-25 18:40:31 +0900

    ✨ Feat: Add code debugger

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+
+import type { Metadata } from "next";
+
+import localFont from "next/font/local";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,9 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
     </html>
   );
 }
