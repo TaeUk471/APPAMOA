3d622b4c8a5a663316b34d4a74e78f303a8304b4
TaeUk471 <asky990223@gmail.com>
2024-09-26 18:21:04 +0900

    ✨ Feat: Make folder Structure and test it

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div>
-      <h1>I found this page</h1>
+      <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
     </div>
   );
