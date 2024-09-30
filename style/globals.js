131016ab6ae60ded00198dfb892cc5f52dd10bcd
TaeUk471 <asky990223@gmail.com>
2024-09-30 14:33:56 +0900

    ✨ Make global.js and reset.js file

---
+++ style/globals.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    html{
+        font-size: 62.5%
+        font-family: "Inter";
+    }
+`;
+
+export default GlobalStyles;
