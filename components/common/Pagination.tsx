e7f345fe4e077e28af37787c718b7884242c999e
TaeUk471 <asky990223@gmail.com>
2024-10-11 15:39:17 +0900

    ✨ Feat: apply usePaginationStore.ts for pagination.tsx

--- components/common/Pagination.tsx
+++ components/common/Pagination.tsx
@@ -1,13 +1,23 @@
+import usePaginationStore from "store/usePaginationStore";
+
 interface PaginationProps {
-  currentPage: number;
+  pageId: string;
   totalPage: number;
-  setCurrentPage: (currentPage: number) => void;
   setTotalPage: (totalPage: number) => void;
 }
 
-export default function Pagination({ currentPage, totalPage, setCurrentPage, setTotalPage }: PaginationProps) {
+export default function Pagination({ pageId, totalPage, setTotalPage }: PaginationProps) {
+  const currentPage = usePaginationStore(state => state.pagination[pageId] || 1);
+  const setPage = usePaginationStore(state => state.setPage);
+
   const pageList = []; // 화면에 렌더링할 페이지 리스트
   const range = 2; // 화면 앞뒤로 보여줄 범위
+
+  const handlePage = (newPage: number) => {
+    if (newPage > 0 && newPage <= totalPage) {
+      setPage(pageId, newPage);
+    }
+  };
 
   for (let i = 1; i <= totalPage; i++) {
     if (i === 1 || i === totalPage || (i >= currentPage - range && i <= currentPage + range)) {
@@ -23,13 +33,13 @@ export default function Pagination({ currentPage, totalPage, setCurrentPage, set
         <button
           className="px-4 py-4 border rounded-lg disabled:opacity-50"
           disabled={currentPage === 1}
-          onClick={() => setCurrentPage(currentPage - 1)}>
+          onClick={() => handlePage(currentPage - 1)}>
           {"<"}
         </button>
         {pageList.map((page, index) => (
           <li key={index} className={`text-lg ${page === currentPage ? "text-blue-400" : ""}`}>
             {typeof page === "number" ? (
-              <button onClick={() => setCurrentPage(page)} className="px-4 py-2">
+              <button onClick={() => handlePage(currentPage)} className="px-4 py-2">
                 {page}
               </button>
             ) : (
@@ -40,7 +50,7 @@ export default function Pagination({ currentPage, totalPage, setCurrentPage, set
         <button
           className="px-4 py-4 border rounded-lg disabled:opacity-50"
           disabled={currentPage === totalPage}
-          onClick={() => setCurrentPage(currentPage + 1)}>
+          onClick={() => handlePage(currentPage + 1)}>
           {">"}
         </button>
         <button className="px-4 py-4 border rounded-lg" onClick={() => setTotalPage(totalPage + 1)}>
