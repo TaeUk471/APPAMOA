c85e255d81501ed60d4301f4daad2059e5599ec9
TaeUk471 <asky990223@gmail.com>
2024-10-14 16:20:17 +0900

    💄 Fix: fix small issue

--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
       {/* Sidebar */}
       <div
         className={`bg-pink-400 fixed top-25 right-0 transition-all
-        ${isOpen ? "w-[400px] h-5/6" : "w-20 h-fit"} flex flex-col`}>
+        ${isOpen ? "w-[400px] h-5/6" : "w-14 h-fit"} flex flex-col`}>
         <div className={`flex justify-between items-center p-4 ${isOpen ? "mb-4" : ""}`}>
           {isOpen && <h1 className="text-white">APPAMOA</h1>}
           <button onClick={() => setIsOpen(!isOpen)} className="cursor-pointer flex items-center justify-center h-8">
