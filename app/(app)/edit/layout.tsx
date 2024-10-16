8a2f14cff56033f1f94c8274dc07ea96594638c0
TaeUk471 <asky990223@gmail.com>
2024-10-16 11:30:42 +0900

    ✨ Feat: Add Drag&Drop provider

--- app/(app)/edit/layout.tsx
+++ app/(app)/edit/layout.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 import Footer from "@components/common/Footer";
 import GNB from "@components/common/GNB";
@@ -9,10 +11,12 @@ export default function EditLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <GNB isEdit={true} />
-      <div className="flex flex-1">
-        <div className="flex-1">{children}</div>
-        <Sidebar />
-      </div>
+      <DndProvider backend={HTML5Backend}>
+        <div className="flex flex-1">
+          <div className="flex-1">{children}</div>
+          <Sidebar />
+        </div>
+      </DndProvider>
       <Footer />
     </div>
   );
