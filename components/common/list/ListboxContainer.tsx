2d8c63aeb75b7cf7ab406b8a5122aab45c475f37
TaeUk471 <asky990223@gmail.com>
2024-11-15 15:04:40 +0900

    ✨ Feat: add page for none-selected state

--- components/common/list/ListboxContainer.tsx
+++ components/common/list/ListboxContainer.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useEffect } from "react";
+
+import useSelectUserStore from "store/useSelectUserStore";
+
 import ListItem from "./ListItem";
 
 // 상수 데이터 정의
@@ -68,21 +74,34 @@ const ListItems = [
 ];
 
 const ListBoxContainer = () => {
+  const selectPatient = useSelectUserStore(state => state.user);
+
+  useEffect(() => {
+    console.log(selectPatient);
+  }, [selectPatient]);
+
   return (
     <div
       className="bg-stone-400 m-1 rounded-lg p-4 overflow-y-auto scrollbar-thin scrollbar-hide h-[100vh]"
       // style={{ maxHeight: "calc(100vh - 100px)" }}
     >
-      {ListItems.map((item, index) => (
-        <ListItem
-          key={index}
-          profileUrl={item.profileUrl}
-          hospital={item.hospital}
-          doctor={item.doctor}
-          date={item.date}
-          result={item.result}
-        />
-      ))}
+      {selectPatient.name !== "" ? (
+        ListItems.map((item, index) => (
+          <ListItem
+            key={index}
+            profileUrl={item.profileUrl}
+            hospital={item.hospital}
+            doctor={item.doctor}
+            date={item.date}
+            result={item.result}
+          />
+        ))
+      ) : (
+        <div className="w-full h-full flex justify-center items-center gap-10">
+          <i className="fa fa-users-slash text-7xl text-stone-200" />
+          <p className="font-poppins text-7xl font-bold text-stone-200">Select User</p>
+        </div>
+      )}
     </div>
   );
 };
